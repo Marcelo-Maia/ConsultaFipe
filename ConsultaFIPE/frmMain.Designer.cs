f9c11e612b107720614d19fdb8f26931c6cd4d0e
ezequiel.daniel <ezequiel.daniel@selbetti.com.br>
2018-06-22 18:42:27 -0300

    Adicionado icone e pacotes de Release

--- ConsultaFIPE/frmMain.Designer.cs
+++ ConsultaFIPE/frmMain.Designer.cs
@@ -28,6 +28,7 @@
         /// </summary>
         private void InitializeComponent()
         {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(frmMain));
             this.groupBox22 = new System.Windows.Forms.GroupBox();
             this.label1 = new System.Windows.Forms.Label();
             this.cmbTipo = new System.Windows.Forms.ComboBox();
@@ -225,6 +226,7 @@
             this.ClientSize = new System.Drawing.Size(573, 245);
             this.Controls.Add(this.groupBox22);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
             this.MaximizeBox = false;
             this.Name = "frmMain";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
